644891e279dfd2ae9e13b58716563b23374cbd0d
Tracy <tracy@mad.geek.nz>
2022-10-05 13:04:36 +1300

    base modal

---
+++ assets/js/about-modal.js
@@ -0,0 +1,9 @@
+$('#about-modal-trigger').on('click', function(e) {
+  $('#about-modal-1').toggleClass("about-modal-show"); //you can list several class names 
+  e.preventDefault();
+});
+
+$('#about-modal-close').on('click', function(e) {
+  $('#about-modal-1').toggleClass("about-modal-show"); //you can list several class names 
+  e.preventDefault();
+});
