12782e993e7b2cbbf561e4ea7fcf6c61f189c635
Tracy <tracy@mad.geek.nz>
2022-11-17 20:00:57 +1300

    converted from jquery into the correct way

--- assets/js/about-modal.js
+++ assets/js/about-modal.js
@@ -1,16 +1,51 @@
 //code for the hamburger bar which include links to navigate round the site.
 
-$("#about-modal-trigger").on("click", function (e) {
-  $("#about-modal-1").toggleClass("about-modal-show"); //you can list several class names
-  e.preventDefault();
+
+// hamburger-bar
+// $("#about-modal-trigger").on("click", function (e) {
+//   $("#about-modal-1").toggleClass("about-modal-show"); //you can list several class names
+//   e.preventDefault();
+// });
+
+let aboutModalTrigger = document.querySelector(`#about-modal-trigger`);
+aboutModalTrigger.addEventListener("click", (e) => {
+
+  let aboutModal = document.querySelector('#about-modal-1');
+  if (aboutModal.className == "about-modal about-modal-effect-1") {
+    aboutModal.className = "about-modal about-modal-effect-1 about-modal-show";
+  } else {
+    aboutModal.className = "about-modal about-modal-effect-1";
+  }
 });
 
-$("#about-modal-close").on("click", function (e) {
-  $("#about-modal-1").toggleClass("about-modal-show"); //you can list several class names
-  e.preventDefault();
+// bug x on modal
+// $("#about-modal-close").on("click", function (e) {
+//   $("#about-modal-1").toggleClass("about-modal-show"); //you can list several class names
+//   e.preventDefault();
+// });
+let aboutModalClose = document.querySelector(`#about-modal-close`);
+aboutModalClose.addEventListener("click", (e) => {
+
+  let aboutModal = document.querySelector('#about-modal-1');
+  if (aboutModal.className == "about-modal about-modal-effect-1") {
+    aboutModal.className = "about-modal about-modal-effect-1 about-modal-show";
+  } else {
+    aboutModal.className = "about-modal about-modal-effect-1";
+  }
 });
 
-$("#ellipsis").on("click", function (e) {
-  $("#ellipsis_menu").toggleClass("ellipsis_menu_show");
-  e.preventDefault();
+// ellipsis button
+// $("#ellipsis").on("click", function (e) {
+//   $("#ellipsis_menu").toggleClass("ellipsis_menu_show");
+//   e.preventDefault();
+// });
+let ellipsisButton = document.querySelector(`#ellipsis`);
+ellipsisButton.addEventListener("click", (e) => {
+
+  let ellipsisMenu = document.querySelector('#ellipsis_menu');
+  if (ellipsisMenu.className == "ellipsis_menu") {
+    ellipsisMenu.className = "ellipsis_menu ellipsis_menu_show";
+  } else {
+    ellipsisMenu.className = "ellipsis_menu";
+  }
 });
